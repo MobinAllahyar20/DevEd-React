c107ea9fa30094986d552a85daf5439b3c1f992e
Mobin <allahyarqasabeimobin@gmail.com>
2022-08-17 00:52:59 +0430

    Commited by mobin

--- delicious/src/App.js
+++ delicious/src/App.js
@@ -1,31 +1,9 @@
-import { useEffect, useState } from "react";
 import Pages from "./pages/Pages";
 
 function App() {
-  const [popular, setPopular] = useState([]);
-
-
-  useEffect(() => {
-    getPopular();
-  }, []);
-
-
-  const getPopular = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
-    const data = await api.json();
-    setPopular(data.recipes);
-  }
-  
-
   return (
     <div>
-      {popular.map((recipes) => {
-        return (
-          <div>
-            <p>{recipe.title}</p>
-          </div>
-        );
-      })}
+      <Pages />
     </div>
   );
 }
